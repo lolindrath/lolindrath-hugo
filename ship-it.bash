9aa3759cde8dec3623547284bed1054434353a57
Andy Williams <Andrew_Williams@Comcast.com>
2018-01-17 10:45:24 -0500

    fix keybase proof

--- ship-it.bash
+++ ship-it.bash
@@ -12,6 +12,9 @@ else
 fi
 
 #&& rsync -avz --delete public/ ${HOST}:${DIR} 
-hugo --baseUrl $BASE_URL && aws s3 sync --acl "public-read" --exclude .DS_Store public/ s3://$BUCKET_NAME  
+hugo --baseUrl $BASE_URL
+mkdir -p public/.well-known/
+cp keybase.txt public/.well-known
+aws s3 sync --acl "public-read" --exclude .DS_Store public/ s3://$BUCKET_NAME  
 
 exit 0
