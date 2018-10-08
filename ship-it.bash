319c69fd00c3d3f14b0335c3c66dfd67bab8c202
Andy Williams <Andrew_Williams@Comcast.com>
2018-10-08 09:21:56 -0400

    add robots.txt generation and rsync to webserver

--- ship-it.bash
+++ ship-it.bash
@@ -1,20 +1,23 @@
 #!/bin/sh
-HOST=lolindrath.com
+#HOST=lolindrath.com
+HOST=web1
 
 if [[ "prod" = $1 ]]; then
 	HUGO_OPTIONS=
 	BASE_URL=https://lolindrath.com/
 	BUCKET_NAME=lolindrath.com
+	DIR=/var/www/lolindrath
 else
 	HUGO_OPTIONS=-DF # drafts and futures will show on Staging
 	BASE_URL=https://staging.lolindrath.com/
 	BUCKET_NAME=staging.lolindrath.com
+	DIR=/var/www/lolindrath-staging
 fi
 
-#&& rsync -avz --delete public/ ${HOST}:${DIR} 
-hugo --baseUrl $BASE_URL $HUGO_OPTIONS
+
 mkdir -p public/.well-known/
 cp keybase.txt public/.well-known
+hugo --baseUrl $BASE_URL $HUGO_OPTIONS && rsync -avz --delete public/ ${HOST}:${DIR}
 aws s3 sync --acl "public-read" --exclude .DS_Store public/ s3://$BUCKET_NAME  
 
 exit 0
